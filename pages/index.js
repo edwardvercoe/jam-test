e54ba5a8932c7bee7d88d09057ad366e86a0b585
Ed <edward.vercoe@gmail.com>
2021-09-29 13:44:00 +1000

    change revalidate time

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export async function getStaticProps() {
     props: {
       recipes: response.items,
     },
-    revalidate: 10, // check/validate if content has changed every 10s
+    revalidate: 60, // check/validate if content has changed every 10s
   };
 }
 
